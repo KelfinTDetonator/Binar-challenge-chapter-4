9d9a14632fd88907633f4fa0eba5fb067d05fb8a
KelfinTDetonator <kelfinantoni@gmail.com>
2023-10-23 18:28:01 +0700

    add: morgan logger

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require('express')
 const app = express()
 const routerUser = require('./src/routes/users.routes');
 const routerAccounts = require('./src/routes/bankAccount.routes');
-const routerTransactions = require('./src/routes/transaction.routes')
+const routerTransactions = require('./src/routes/transaction.routes');
 
 const swaggerUi = require('swagger-ui-express')
 const swaggerJson = require('./openApi.json')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
 // const routerAcc = require('./routers/accounts/accountsRouter')
 // const hostname = '127.0.0.1';
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.json({strict: false}));
+app.use(morgan('dev'))
 
 app.use("/api/v1/documentation", swaggerUi.serve, swaggerUi.setup(swaggerJson))
 
