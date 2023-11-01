977d8ab64ba1d48a897ee28e1a844318a0a340ef
KelfinTDetonator <kelfinantoni@gmail.com>
2023-11-01 18:37:49 +0700

    fix: typo at users.routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const userController = require('../users/userController');
 const checkToken = require('../middleware/checkToken');
+const validator = require('../middleware/validation');
+const validation = require('../validation/authValidation')
 
 router.get('/', userController.getAllUsers)
-router.post('/auth/register', userController.registerUser)
-router.post('/auth/login', userController.loginUser)
+router.post('/auth/register', validator(validation.isRegisterValid), userController.registerUser)
+router.post('/auth/login', validator(validation.isLoginValid), userController.loginUser)
 router.get('/auth/authenticate', checkToken, userController.userAuthenticated)
 router.put('/:id', userController.updateUserById)
 router.patch('/:id', userController.patchUserById)
